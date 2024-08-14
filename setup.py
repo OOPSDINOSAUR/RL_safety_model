38cda23f3f4be01110f4fd8a8854e998fa9d3d1a
DINOSAUR <1768430958@qq.com>
2024-08-15 01:42:31 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='RL_safety_model',
+      version='1.0',
+      description='Conservative Q learning based RL safety models for variable length time series',
+      author='Tu Rui',
+      author_email='turui@my.swjtu.edu.cn',
+      py_modules=[]
+     )
